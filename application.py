28eaa2596700a9bae412d896f15ba0f67144bcc0
olyakovlev <ole.yakovlev@gmail.com>
2019-05-21 06:36:52 -0500

    Custom routes

--- application.py
+++ application.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, world!"
+
+@app.route("/<string:name>")
+def name(name):
+    name = name.capitalize()
+    return f"Hello, {name}!"
